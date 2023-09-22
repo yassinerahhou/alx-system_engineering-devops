9491826af04024abe71ff761fb398060d1d0a540
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-22 12:58:56 -0700

    x

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 file { '/tmp/school':
-  ensure  => 'file',        # Ensure that the path is a file, not a directory
-  owner   => 'www-data',    # Replace with the desired owner's username
-  group   => 'www-data',    # Replace with the desired group name
-  mode    => '0744',        # Correct file permission mode
-  content => 'I love Puppet',  # Specify the file content here
+ensure  => '/tmp/school',
+mode    => '0744',
+
+owner   => 'www-data',   # Replace with the desired owner's 
+group   => 'www-data',  # Replace    => '0744',
+content => 'I love Puppet',  # Specify the file content here
+
 }
