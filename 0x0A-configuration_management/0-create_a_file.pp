164b5247763d3616bda499f20114668178d75e10
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-22 13:02:39 -0700

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,7 @@
-# Replace with the desired
-file { '/tmp/school':
-ensure  => '/tmp/school',
+# create a file in /tmp.
+file { '/tmp/school' :
 mode    => '0744',
-
-owner   => 'www-data',   # Replace with the desired owner's 
-group   => 'www-data',  # Replace    => '0744',
-content => 'I love Puppet',  # Specify the file content here
-
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
