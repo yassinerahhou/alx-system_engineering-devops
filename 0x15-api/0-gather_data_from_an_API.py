9ec6189b5bdcf21e17bac3129cf940ecdd1cdcbe
Yassine rahhou <yassine.rahhou@gmail.com>
2023-11-04 20:14:29 +0000

    test

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/" + argv[1]).json()
+    # Send an HTTP GET request to retrieve a list of tasks (todos) for the same user
+    tasks = requests.get(url + "todos?userId="+str(argv[1])).json()
+
+    completed = []
+    for i in tasks:
+        if i.get("completed") is True:
+            completed.append(i.get("title"))
+
+    # "brahim"  # EMPLOYEE_NAME: name of the employee
+    EMPLOYEE_NAME = user.get("name")
+    NUMBER_OF_DONE_TASKS = len(completed)  # number of completed tasks
+
+    # number of completed tasks [ complet +non coomplete] task
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
+
+    # TASK_TITLE = "task title"
+
+    print(
+        f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for TASK_TITLE in completed:
+        print(f"\t {TASK_TITLE}")
